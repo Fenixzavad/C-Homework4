572764c50f060f6a60c3d6531bb3bb7ff982ef68
fenixzava <fenixzava@gmail.com>
2022-10-18 00:08:47 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Функция должна на вход принимать число, а выдавать сумму его цифр
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumeralsNumber(int number)
+{   
+    if(number < 0)
+    {
+        number=-number;
+    }
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+int answer = SumNumeralsNumber(number);
+System.Console.WriteLine($"Сумма разрядов(цифр) числа {number} равна {answer}");
+
