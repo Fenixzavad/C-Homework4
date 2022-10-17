f283227d57587a6bf7f49653a0ac64fb34715213
fenixzava <fenixzava@gmail.com>
2022-10-18 01:09:24 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 7, 8
+
+ int [] WriteArray()
+ {
+ int [] array = new int [8];
+ 
+ for(int i = 0; i < array.Length; i++)
+ {
+    array[i] = new Random().Next(0,30);
+    System.Console.Write($" {array [i]}");
+ }
+ return array;
+ }
+
+int [] answer = WriteArray();
+  
